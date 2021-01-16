4d19e8ad43735053e1248538a88bdd345fa58e22
Emirhan Latif <emirhanlatif@outlook.com>
2021-01-16 23:49:44 +0300

    16 Ocak Ders Sonu

---
+++ ConsoleApp(DegerveReferansTipler)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp_DegerveReferansTipler_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi1 = 65;
+            //sayi1=30 olur
+        }
+    }
+}
---
+++ ConsoleApp1(Class)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1_Class_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            araba car1 = new araba();
+            car1.marka = "Toyota";
+            car1.model = "Corolla";
+            car1.tarih = 2011;
+            car1.renk = "Kırmızı";
+
+            Console.WriteLine(car1.marka);
+
+        }
+    }
+
+    class araba
+    {
+        public string marka { get; set; }
+        public string model { get; set; }
+        public int tarih { get; set; }
+        public string renk { get; set; }
+
+
+    }
+}
